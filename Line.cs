bda82bb9bbb8ce56c3c52f2870ee4776996bcd73
siriusamro <73645885+siriusamro@users.noreply.github.com>
2022-07-28 03:06:32 +0200

    Add project files.

---
+++ Line.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lasso_tool
+{
+    public class Line
+    {
+        public Point p1, p2;
+
+        public Line(Point p1, Point p2)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+
+        public SortedDictionary<int, List<int>> getPointsFromDrawing(HashSet<Point> drawnPoints)
+        {
+            SortedDictionary<int, List<int>> points = new SortedDictionary<int, List<int>>();
+            for (int i = 0; i < drawnPoints.Count-1; i++)
+            {
+                Point a = drawnPoints.ElementAt(i);
+                Point b = drawnPoints.ElementAt(i+1);
+
+            }
+            return points;
+        }
+
+
+        public SortedDictionary<int, List<int>> getPoints()
+        {
+            //var points = new Point[Math.Max(p2.X-p1.X,p2.Y-p1.X) + 10];
+            int quantity = (int) Math.Max(p2.X - p1.X, p2.Y - p1.X) + 10;
+            SortedDictionary<int,List<int>> points = new SortedDictionary<int, List<int>>();
+            
+            int ydiff = p2.Y - p1.Y, xdiff = p2.X - p1.X;
+            double slope = (double)(p2.Y - p1.Y) / (p2.X - p1.X);
+            double x, y;
+
+            for (double i = 0; i < quantity; i++)
+            {
+                y = slope == 0 ? 0 : ydiff * (i / quantity);
+                x = slope == 0 ? xdiff * (i / quantity) : y / slope;
+                addPointOnLineDictionary(new Point((int)Math.Round(x) + p1.X, (int)Math.Round(y) + p1.Y),points);
+                
+            }
+            addPointOnLineDictionary(new Point(p2.X,p2.Y), points);
+            return points;
+        }
+
+        private void addPointOnLineDictionary(Point point, SortedDictionary<int, List<int>> points)
+        {
+            if (points.ContainsKey(point.X))
+            {
+                points[point.X].Add(point.Y);
+                points[point.X].Sort();
+            } else points.Add(point.X, new List<int> { point.Y });
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics; 
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.Storage;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace lasso_tool
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        bool isPressed = false;
+        Point a = new Point(-1,-1);
+        List<Point> points = new List<Point>();
+        WriteableBitmap imageWriteableBitmap;
+        Double minX = Double.MaxValue,minY = Double.MaxValue, maxX = Double.MinValue, maxY= Double.MinValue;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //WriteableBitmap writeableBmp = BitmapFactory.New(512, 512);
+            this.Loaded += MainPage_Loaded;
+            
+           
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //imageWriteableBitmap = new WriteableBitmap((int)lassoImg.ActualWidth, (int)lassoImg.ActualHeight);
+            imageWriteableBitmap =  await BitmapFactory.FromContent(new Uri(this.BaseUri, "/Assets/x.png"));// BitmapFactory.New((int)lassoImg.ActualWidth, (int)lassoImg.ActualHeight);//.FromByteArray(ImageUriToBytes(@"C:\Users\amrow\OneDrive\الصور\Cyberpunk 2077\photomode_01082021_192634.png").Result);
+            //imageWriteableBitmap.Clear(Colors.Beige);
+            //imageWriteableBitmap.FillRectangle(150, 150, 250, 250, Colors.White);
+            lassoImg.Source = imageWriteableBitmap;
+            //writeableBmp.Blit()
+            //lassoImg.Source = imageWriteableBitmap;
+        }
+
+        private void Image_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            Debug.WriteLine("Inside");
+            isPressed = true;
+           
+        }
+
+        private void Image_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            var point = e.GetCurrentPoint(sender as Image).Position;
+            
+
+            if (isPressed)
+            {
+                minX = Math.Min(minX, point.X);
+                minY = Math.Min(minY, point.Y);
+                maxX = Math.Max(maxX, point.X);
+                maxY = Math.Max(maxY, point.Y);
+
+                if (a.X!=-1)
+                {
+                    imageWriteableBitmap.DrawLine((int)a.X, (int)a.Y, (int)point.X, (int)point.Y, Colors.Black);
+                    Debug.WriteLine("Moving " + point.X + " " + point.Y);
+                }
+                a = point;
+            }
+        }
+
+        private async void Image_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            isPressed = false;
+            a.X = -1;
+            //imageWriteableBitmap.FillRectangle(150, 150, 250, 250, Colors.White);
+            BitmapImage image = new BitmapImage();
+
+            WriteableBitmap c = BitmapFactory.New((int)lassoImg.ActualWidth, (int)lassoImg.ActualHeight);// await BitmapFactory.FromContent(new Uri(this.BaseUri,"/Assets/x.png"));
+            c.Clear(Colors.White);
+            c.Blit(new Rect(new Point(minX, minY), new Point(maxX,maxY)), imageWriteableBitmap, new Rect(new Point(minX, minY), new Point(maxX, maxY)));
+            lassoImg.Source = c;
+            //c.Clear(Colors.AliceBlue);
+            //c.Blit(new Rect(new Point(300,300),new Size(500,500)), imageWriteableBitmap, new Rect(100, 100, 300, 300));
+            //lassoImg.Source = c;
+            //imageWriteableBitmap.
+            minX = Double.MaxValue; minY = Double.MaxValue; maxX = Double.MinValue; maxY = Double.MinValue;
+
+        }
+
+
+        
+    }
+}
